5a824d0ce77905bba3666c611640c274cdfd744c
Vineesh Kalarickal <vineesh.kalarickal@gmail.com>
2020-02-17 13:58:32 +0530

    vin : updated on web
    added new line "pull req etc..."

--- MyNewFile.php
+++ MyNewFile.php
@@ -3,4 +3,6 @@ echo "https://github.com/vineeshkalarickal/studygit.git";
 echo "Git tutorial";
 /* adding new comments */
 echo "Name : Vineesh Kalarickal";
+/* this change is done in git hub dot com - pull req required*/
+echo "Pull req will show this line.";
 ?>
